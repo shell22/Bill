492c8aff1ef623c871ad441870b923ccb13acae8
GuDongBin <backnig@163.com>
2019-06-30 22:17:22 +0800

    修正

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public  class Hello{
   public static void main(String[] args){
      System.out.println("Hello World"); 
-     System.out.println("你好，世界");
+     System.out.println("你好啊，世界");
   }
 }
